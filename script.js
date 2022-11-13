5ab99acdec890966b0777b55de34ebffb3f74f14
yoavavraham00 <114678073+yoavavraham00@users.noreply.github.com>
2022-11-14 00:04:52 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let kipkip = [];
+let a = prompt("enter something please");
+for (let i = 0; i < 10; i++) {
+  kipkip.push(prompt(a[i]));
+  console.log(kipkip);
+}
+alert("you just gave us" + kipkip);
